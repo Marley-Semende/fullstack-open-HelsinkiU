ec697fc0e0723982619aef651a1330a05029ce1b
Marley-Semende <marley.semende@gmail.com>
2024-06-10 23:50:37 +0200

    Completed part2 exercise 2.4

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,38 +1,42 @@
 const Course = ({ course }) => {
   return (
     <div>
-      <Header course={course} />
+      <Header name={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
   );
 };
-export default Course;
 
-//sub components
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((total, part) => total + part.exercises, 0);
+  return (
+    <p>
+      <strong>Total number of exercises: {sum}</strong>
+    </p>
+  );
 };
 
 const Part = ({ part }) => {
   return (
     <p>
-      {part.name} {part.exercises}
+      {part.name}: {part.exercises}
     </p>
   );
 };
 
 const Content = ({ parts }) => {
   return (
-    <>
+    <div>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
-    </>
+    </div>
   );
 };
 
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {total}</p>;
-};
+export default Course;
